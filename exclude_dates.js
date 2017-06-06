2c58615bc97c19280a643cba6f2c1e3c230dbd3c
Tyler Rowsell <tyler.rowsell@shopify.com>
2017-06-06 16:45:00 -0230

    Update exclude_dates.js

--- exclude_dates.js
+++ exclude_dates.js
@@ -1,32 +1,17 @@
-//Exclude These Dates from Weekend
-var dateExceptions = ["14/05/2017", "13/05/2017"];
-$(document).ready( function() {
-  $(function() {
-    $("#date").datepicker( {
-      dateFormat: 'dd/mm/yy',
-      minDate: 0,
-      maxDate: '+2M',
-      beforeShowDay: function(date) {
-        //check if currentDate is part of dateExceptions
-        var show = false;
-        for(i=0;i<dateExceptions.length;i++){
-          var f = new Date(dateExceptions[i].split("/")[2], (dateExceptions[i].split("/")[1]-1), dateExceptions[i].split("/")[0]);
-          if (date.getMonth() == f.getMonth() && date.getYear() == f.getYear() && date.getDate() == f.getDate()){
-            show = true;
-          }
-        }
-        // remove weekends (day 0/6) if date isn't excluded.
-        day = date.getDay();
-        if ( day == 0 || day == 6 ) {
-          if(show){
-          return [true, ""]
-          }else{
-          return [false, ""]            
-          }
-        } else {
-          return [true, ""]
-        }
-      }
-    } );
-  });
+var array = ["2017-06-07","2017-06-08" ,"2017-06-09"];
+checkDate = function(date){
+  if($.inArray($.datepicker.formatDate('yy-mm-dd', date ), array) > -1){
+   return [false]
+  }else{
+   var day = date.getDay();
+    if(day == 0 || day == 6){
+     return [false]
+    }else{
+      return [true]
+    }
+  }
+}
+
+$('#date').datepicker({
+  beforeShowDay: checkDate,
 });
