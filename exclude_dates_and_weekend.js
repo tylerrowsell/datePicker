ee3c9e5b858c7bbeb899b79c95b1836303919fdb
Tyler Rowsell <tyler.rowsell@shopify.com>
2017-06-06 16:49:35 -0230

    Update exclude_dates_and_weekend.js

--- exclude_dates_and_weekend.js
+++ exclude_dates_and_weekend.js
@@ -1,11 +1,14 @@
-var array = ["2017-06-07","2017-06-08" ,"2017-06-09"];
+var excludeArray = ["2017-06-07","2017-06-08" ,"2017-06-09"];
+var includeArray = ["2017-06-04"];              
 checkDate = function(date){
-  if($.inArray($.datepicker.formatDate('yy-mm-dd', date ), array) > -1){
-   return [false]
+  if($.inArray($.datepicker.formatDate('yy-mm-dd', date ), excludeArray) > -1){
+    return [false]
+  }else if($.inArray($.datepicker.formatDate('yy-mm-dd', date ), includeArray) > -1){
+    return [true]
   }else{
-   var day = date.getDay();
+    var day = date.getDay();
     if(day == 0 || day == 6){
-     return [false]
+      return [false]
     }else{
       return [true]
     }
